998dedac9655775288dc3503dcb30c03cfa51a86
Chinmay Sharma <c1sharma@ucsd.edu>
2024-05-31 19:39:00 -0700

    added upload photo functionality

--- rag_llama3_with_history_streamlit.py
+++ rag_llama3_with_history_streamlit.py
@@ -1,16 +1,28 @@
 import streamlit as st
+from PIL import Image
 from langchain_core.messages import HumanMessage, AIMessage
 from rag_llama3_with_history import RAG
 
 
 if __name__ == '__main__':
-    st.set_page_config(page_title="DermAssist", page_icon="🤖")
+    st.set_page_config(page_title="DermAssist", page_icon="⚕️")
     st.title("DermAssist")
 
     rag = RAG()
 
+    uploaded_file = st.file_uploader("Upload an image of the affected area", type=["jpg", "jpeg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        col1, col2, col3 = st.columns([1, 2, 1])
+        with col2:
+            st.image(image, caption="Uploaded Image", use_column_width=True)
+        st.write("Filename:", uploaded_file.name)
+    else:
+        st.info("Please upload an image to continue")
+        st.stop()
+
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = []
+        st.session_state.chat_history = [AIMessage(content="You are suffering with acne")]
 
     for message in st.session_state.chat_history:
         if isinstance(message, HumanMessage):
